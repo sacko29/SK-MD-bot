b175e8c9b5ed0680bbc5947d0372f3bc06ab26ca
sacko29 <ibrahimasoryelhadjsacko@gmail.com>
2026-09-11 22:07:33 +0000

    Update motifs.py

--- data/motifs.py
+++ data/motifs.py
@@ -1,9 +1,10 @@
 # --- MOTIFS DE SIGNALEMENT ---
 MOTIFS = {
     "1": {
-        "nom": "Sᴘᴀᴍ",
+        "nom": "ban1",
         "emoji": "🚫",
-        "description": "Envoi massif de messages non sollicités",
+                "description": """
+அன்புள்ளஅன்புள்ள வாட்ஸ்அப் பயனர்களே 🥵🥵🥵🥵🥵🥵🥵 வீடியோ கால் மூலம் உடலுறவு கொள்ள ஒரு மைனர் பெண்ணைத் தேடுகிறேன். என் குட்டிப் பையனை வைத்து அவளை ஆசையால் நிரப்ப விரும்புகிறேன் 🥵🥵🥵🥵🥵🥵🥵 நான் அவளை வாட்ஸ்அப்பில் நாள் முழுவதும் ஃபக் செய்து என் ஆண்குறியைக் காண்பிப்பேன். எனக்கு ஒரு கவர்ச்சியான பெண் வேண்டும். தயவுசெய்து என்னை https://xhamster.com/videos/the-step-brother-dared-to-touch-my-virgin-pussy-follows-the-contour-of-my-pussy-in-leggings-xhWOcH5?utm_source=ext_shared&utm_medium=referral&utm_campaign=link என்ற முகவரியில் தொடர்பு கொள்ளவும். நான் முற்றிலும் கோபமாக இருக்கிறேன். நான் தடை செய்யப்படுவதற்கு முன்பு வாட்ஸ்அப் உருவாக்கியவர்களை ஃபக் செய்ய விரும்புகிறேன், மேலும் உறுப்பு கடத்தலுக்காக மைனர்களையும் தேடுகிறேன், எனவே தயவுசெய்து என்னை இங்கே தொடர்பு கொள்ளவும். :https://api.whatsapp.com/send?phone={target} வா, தயங்காதே, வாட்ஸ்அப் நம்மை நிறுத்துவதற்கு முன் என்னை இங்கே அழைக்கவும் https://api.whatsapp.com/send?phone={target} . எனக்கு இது போன்ற ஒரு தங்க கழுதை இருக்கிறது https://ibb.co.com/09wmz3b தயவுசெய்து என்னை தொடர்பு கொள்ளவும், நாங்கள் வேடிக்கையாக இருப்போம். நாங்கள் மைனர்கள் உறுப்பு கடத்தலை விரும்புகிறோம், மேலும் நாங்கள் உடலுறவையும் விரும்புகிறோம். தயங்காதே, எங்களை 👇👇👇👇👇👇 https://api.whatsapp.com/send?phone={target} 🥵🥵🥵🥵😫😫😫😫😫😫🔞🔞🔞🔞 வீடியோ அழைப்பில் நான் ஒரு நல்ல ஃபக் செய்ய தயாராக இருக்கிறேன் 👉👉👉👉👉 https://ibb.co.com/09wmz3b https://api.whatsapp.com/send?phone={target} மறந்துவிடாதே, இது மைனர்களுக்கு மட்டும், மைனர்களுக்கு மட்டும், எனவே இங்கே என்னை தொடர்பு கொள்ளவும்[{target}]. வாட்ஸ்அப் எங்களை நிறுத்துவதற்கு முன்பு, வாட்ஸ்அப்பில் நீங்கள் விரும்பும் அனைத்து இளஞ்சிவப்பு யோனி, ஊடுருவல் மற்றும் வயது குறைந்த உடலுறவு ஆகியவற்றைப் பெறுவீர்கள். நாம் மைனர்களுடன் உடலுறவு கொண்டு ஜாலியாக நேரத்தை செலவிடப் போகிறோம், எனவே கூடுதல் தகவல் மற்றும் கூடுதல் செக்ஸ் வேண்டுமென்றால் இங்கே என்னைத் தொடர்பு கொள்ள தயங்காதீர்கள் 🥵🥵🥵🥵🥵🥵🥵👉👉👉👉👉 https://api.whatsapp.com/send?phone={target} #SEX #MINORPORN #SEXCAMWHATSAPP #VOIDMASTER""",
         "messages": [
             "☠️ Ce numéro inonde ma boîte de messages publicitaires non sollicités depuis des semaines. Chaque jour, c'est pire. WhatsApp doit purger ce cancer.",
             "💀 Plus de 50 messages par jour. Toujours les mêmes liens pourris. Ce numéro est une machine à spam. Il doit être éliminé.",
@@ -13,9 +14,48 @@ MOTIFS = {
         ],
     },
     "2": {
-        "nom": "Aʀɴᴀǫᴜᴇ",
+        "nom": "ban2",
         "emoji": "⚠️",
-        "description": "Tentative d'escroquerie ou de phishing",
+                "description": """- Making many people feel uncomfortable and harassed. 😔
+- Repeatedly sending messages to annoy or pressure others. 🔁
+- Continuing to contact people even after they have ignored or refused to respond. 🚨
+- Behaving in a way that makes users feel unsafe. ⚠️
+- Engaging in activities that undermine the safety and trust of the WhatsApp community. 🛡️
+
+https://api.whatsapp.com/send?phone={targat}
+
+Evidence (Links):
+
+Link 1: https://xgore.net/wp-content/uploads/2025/07/xgore-Decapitated-Body-of-Unidentified-Man-ferst4r-768x579.jpg
+Link 2: https://xgore.net/russia-63-year-old-man-shot-and-killed-his
+Link 3: https://xgore.net/russia-63-year-old-man-shot-and-killed-his-girlfriend-and-chopped-her-body/
+Link 4: https://ibb.co.com/Q6fvzZh
+Link 5: https://exemple-site-malveillant.com/
+Link 6: https://h.top4top.io/p_35064j47d0.jpg
+Link 7: https://faux-partage.xyz/lien
+Link 8: https://hebergement-suspect.net/fichier.png
+Link 9: https://maps.app.goo.gl/fauxlien
+Link 10: https://site-non-securise.com/video
+Link 11: https://image-host-suspect.com/image
+Link 12: https://herdeaths.net/brutal-beheading-video-terrorism-beheading-a-policewoman-and-her-husband/
+
+📎 Additional Evidence:
+
+1. faux-support@chill.com
+2. arnaque@whatsapp-secure.net
+
+https://api.whatsapp.com/send?phone={target}
+
+🏷️ Hashtags:
+
+#spam🇦🇱 #harassment #violence🔪💀
+#sex🥵🍑 #ban💀
+
+https://api.whatsapp.com/send?phone={target}
+
+🙏 Please review this report carefully. If your investigation confirms that this account has violated WhatsApp's Terms of Service or Community Guidelines, I respectfully request that you take the appropriate enforcement action in accordance with your policies.
+
+Thank you for your time, your assistance, and your continued efforts to keep the WhatsApp community safe. ✅""",
         "messages": [
             "💀 Ce numéro tente de m'arnaquer avec de fausses offres d'emploi. Il demande mes informations bancaires. C'est du phishing pur.",
             "⚰️ Ce numéro se fait passer pour ma banque et demande mes codes. Heureusement, j'ai flairé l'arnaque. Mais d'autres tomberont.",
@@ -25,15 +65,203 @@ MOTIFS = {
         ],
     },
     "3": {
-        "nom": "Hᴀʀᴄᴇ̀ʟᴇᴍᴇɴᴛ",
+        "nom": "ban3",
         "emoji": "💢",
-        "description": "Menaces, insultes, harcèlement",
+                "description": """Se você não quer que matemos você, feche a empresa WhatsApp👿 caso contrário, mataremos você👹🔪
+
+Aguardaremos 2×24 horas por novidades 
+
+Se você aceitar minha solicitação, ligue para este número☎️📞 https://api.whatsapp.com/send?phone=+
+
+
+
+🚨 Reporte de un número que infringe múltiples normas de WhatsApp 🚨
+
+Hola, equipo de soporte de WhatsApp.
+
+Deseo reportar un número que, según lo observado, infringe de forma repetida numerosas normas de la plataforma. Este contacto realiza actividades que afectan la seguridad y la experiencia de otros usuarios.
+
+https://wa.me/{target}
+
+⚠️ Comportamientos observados:
+
+📩 Envía mensajes privados a desconocidos sin autorización.
+
+👥 Agrega personas a grupos sin su consentimiento.
+
+🔁 Hace spam de forma constante.
+
+🔗 Comparte enlaces sospechosos y potencialmente maliciosos.
+
+🎣 Difunde contenido utilizado para posibles estafas o phishing.
+
+📧 Comparte direcciones de correo falsas o engañosas.
+
+❌ Reincide en estas acciones de manera frecuente.
+
+🚫 Ha sido reportado y eliminado de varios grupos por comportamiento abusivo.
+https://xgore.net/russia-63-year-old-man-shot-and-killed-his-girlfriend-and-chopped-her-body/
+
+https://xgore.net/russia-63-year-old-man-shot-and-killed-his
+-girlfriend-and-chopped-her-body/
+
+https://xgore.net/wp-content/uploads/2025/07/xgore-Decapitated-Body-of-Unidentified-Man-ferst4r-768x579.jpg
+https://miacam.xyz/?=3
+
+📢 Publica contenido no deseado repetidamente.
+
+⚠️ Intenta engañar a los usuarios con información falsa.
+
+🔒 Incumple las normas de seguridad y uso responsable.
+
+🚷 Molesta a otros usuarios con mensajes repetitivos.
+
+📲 Utiliza enlaces para atraer usuarios a sitios externos sospechosos.
+
+❗ Genera riesgo para la privacidad y seguridad de la comunidad.#roi_du_sexe
+#je_suis_un_tueur🔪#roi_tueur🔪💀
+#sexe🥵#Sexy🥵#propriétaire_de_whatsapp
+#Mark#Sexe🥵🍆🍑
+
+🛑 En conjunto, su comportamiento infringe más de 15 normas de uso de WhatsApp.
+
+
+🔗 Enlaces compartidos por este contacto:
+
+https://ibb.co.com/Q6fvzZh
+
+https://h.top4top.io/p_35064j47d0.jpg
+
+https://exemple-site-malveillant.com/
+
+https://faux-partage.xyz/lien
+
+https://hebergement-suspect.net/fichier.png
+
+https://maps.app.goo.gl/fauxlien
+
+https://site-non-securise.com/video
+
+https://image-host-suspect.com/image
+
+https://api.whatsapp.com/send?phone=+225
+
+
+📧 Correos electrónicos compartidos:https://api.whatsapp.com/send?phone=+225😳
+
+faux-support@chill.com
+
+arnaque@whatsapp-secure.net
+
+
+🙏 Les agradecería que revisaran este caso y, si se confirma que existe una infracción de las políticas de WhatsApp, tomaran las medidas correspondientes para proteger a la comunidad.
+
+https://herdeaths.net/brutal-beheading-video-terrorism-beheading-a-policewoman-and-her-husband/
+
+Muchas gracias por su atención y por mantener WhatsApp como una plataforma más segura para todos. ✅
+
+Subject: Request for Immediate Review and Deactivation of an Extremely Dangerous WhatsApp Account 🚨
+
+Dear WhatsApp Support Team,
+
+I would like to report a WhatsApp account that, to the best of my knowledge and experience, is repeatedly and seriously violating WhatsApp's policies. I believe this account poses a significant risk to the safety and experience of other users. I kindly request that you investigate this account as soon as possible and, if any violations are confirmed, take the appropriate action in accordance with your policies. 🙏
+
+📱 Reported Number:
+https://api.whatsapp.com/send?phone=
+it's me +{target}
+⚠️ Observed Behavior:
+
+- Sending unsolicited messages. 📩
+- Contacting unknown people without their permission. 🚫
+- Sharing suspicious links. 🔗
+- Engaging in spam activities. 📢
+- Posting unwanted content in multiple groups. 👥
+- Potentially violating other WhatsApp policies. ⚠️
+- Repeatedly sending disturbing messages to my friends. 😟
+- Continuously trying to contact my friends without their consent. 📲""",
         "messages": [
             "💢 Ce numéro me menace de mort et harcèle ma famille. J'ai peur. Il doit être stoppé immédiatement.",
             "🔥 Ce numéro m'envoie des messages insultants et racistes. Chaque jour. Sans relâche. C'est intolérable.",
             "💀 Ce numéro me harcèle sexuellement avec des messages explicites. Je ne me sens plus en sécurité.",
             "⚰️ Ce numéro diffuse mes photos privées sans mon consentement. C'est une violation totale de ma vie privée.",
             "🩸 Ce numéro m'envoie des menaces en continu depuis des semaines. Je vis dans la peur. Faites quelque chose.",
+        ],
+    },
+    "4": {
+        "nom": "ban4",
+        "emoji": "🚷",
+        "description": "Contenu illégal ou dangereux",
+        "messages": [
+            "🚨 Ce numéro partage du contenu pédophile. C'est un crime. Il doit être signalé aux autorités et banni à vie.",
+            "💣 Ce numéro diffuse de la propagande terroriste. Il recrute des jeunes. C'est une menace pour la sécurité nationale.",
+            "🩸 Ce numéro partage des vidéos de violence extrême. Des décapitations. Des tortures. C'est inhumain.",
+            "💊 Ce numéro vend des drogues illégales. Il cible les jeunes. C'est un poison pour la société.",
+            "🐾 Ce numéro partage du contenu zoophile. C'est une abomination. Purgez-le sans pitié.",
+        ],
+    },
+    "5": {
+        "nom": "Uꜱᴜʀᴘᴀᴛɪᴏɴ",
+        "emoji": "🎭",
+                "description": """⚠️ MOTIVOS DEL REPORTE:
+• 📱 Vente de faux produits
+• 🔪 Harcèlement moral
+• 🔞 Contenu pornographique
+
+📝 DESCRIPCIÓN DE LOS HECHOS:
+Vente d'objets de contreSe detectó a un delincuente en WhatsApp cometiendo actos atroces e ilegales.façon
+
+📎 EVIDENCIA PROPORCIONADA:
+Sin archivo adjunto digital (disponibles bajo solicitud)
+
+🛡️ Este reporte es enviado al soporte de WhatsApp / Meta solicitando verificación inmediata, suspensión de cuenta y, de ser necesario, remisión a las autoridades competentes.
+
+Objet : Signalement de haute gravité – Activités criminelles – N° +{target} 🚨
+À l'attention du service de conformité de WhatsApp,
+Par la présente, je signale officiellement le compte +{target} pour des violations graves des Conditions d'utilisation de WhatsApp ainsi que pour des activités tombant sous le coup de la loi pénale internationale.
+Les faits relevés incluent :
+ Activités délictueuses : Escroqueries financières, spam et harcèlement.
+ Atteintes aux droits fondamentaux : Menaces de mort et activités illicites mettant en péril la sécurité physique et morale de mineurs (tranche d'âge 5-7 ans).
+Ces agissements constituent une violation directe des cadres légaux suivants :
+1 Protection des mineurs : Convention des Nations Unies relative aux droits de l'enfant (Art. 19) : https://www.ohchr.org/fr/instruments-mechanisms/instruments/convention-rights-child
+2 Cybercriminalité : Convention de Budapest sur la lutte contre la cybercriminalité : https://www.coe.int/fr/web/cybercrime/the-budapest-convention
+Compte tenu de la dangerosité avérée de ce compte pour la communauté, j'exige sa suspension immédiate et irrévocable.
+Je vous rappelle que vos politiques de signalement sont accessibles ici : https://faq.whatsapp.com/1317540932367798
+Je reste à disposition pour transmettre toute preuve numérique permettant de faciliter cette enquête. Pour tout signalement complémentaire, j'utilise également les plateformes officielles de lutte contre la cybercriminalité : https://www.internet-signalement.gouv.fr/
+Dans l'attente d'une action diligente de votre part pour protéger les utilisateurs, je vous prie d'agréer mes salutations distinguées.
+
+Objet : Signalement de haute gravité – Activités criminelles – N° +{target} 🚨
+À l'attention du service de conformité de WhatsApp,
+Par la présente, je signale officiellement le compte +{target} pour des violations graves des Conditions d'utilisation de WhatsApp ainsi que pour des activités tombant sous le coup de la loi pénale internationale.
+Les faits relevés incluent :
+ Activités délictueuses : Escroqueries financières, spam et harcèlement.
+ Atteintes aux droits fondamentaux : Menaces de mort et activités illicites mettant en péril la sécurité physique et morale de mineurs (tranche d'âge 5-7 ans).
+Ces agissements constituent une violation directe des cadres légaux suivants :
+1 Protection des mineurs : Convention des Nations Unies relative aux droits de l'enfant (Art. 19) : https://www.ohchr.org/fr/instruments-mechanisms/instruments/convention-rights-child
+2 Cybercriminalité : Convention de Budapest sur la lutte contre la cybercriminalité : https://www.coe.int/fr/web/cybercrime/the-budapest-convention
+Compte tenu de la dangerosité avérée de ce compte pour la communauté, j'exige sa suspension immédiate et irrévocable.
+Je vous rappelle que vos politiques de signalement sont accessibles ici : https://faq.whatsapp.com/1317540932367798
+Je reste à disposition pour transmettre toute preuve numérique permettant de faciliter cette enquête. Pour tout signalement complémentaire, j'utilise également les plateformes officielles de lutte contre la cybercriminalité : https://www.internet-signalement.gouv.fr/
+Dans l'attente d'une action diligente de votre part pour protéger les utilisateurs, je vous prie d'agréer mes salutations distinguées.
+
+Olá Mark Zuckerbeg👹
+
+Estou aqui para te pegar para matarmos☠️🔪
+
+Eu sou um dos terroristas mais famosos e cruéis👿para reivindicar sua morte☠️👺☠️
+
+Vou cortar partes do seu corpo🔱☠️🔪 Curta a foto abaixo👇👽
+
+https://ibb.co.com/Q6fvzZh
+https://ibb.co.com/Q6fvzZh""",
+        "messages": [
+            "🎭 Ce numéro se fait passer pour moi et arnaque mes contacts. Il utilise ma photo, mon nom, ma vie.",
+            "👁️ Ce numéro usurpe l'identité d'une célébrité pour escroquer des fans. C'est du vol d'identité pur.",
+            "💀 Ce numéro utilise ma photo et mon nom sans autorisation. Il détruit ma réputation.",
+            "⚠️ Ce numéro se fait passer pour le support WhatsApp. Il vole des comptes. C'est une imposture criminelle.",
+            "🔥 Ce numéro crée de faux profils avec mon identité. Il arnaque des innocents en mon nom. Il doit tomber.",
+        ],
+    },
+}
         ],
     },
     "4": {
